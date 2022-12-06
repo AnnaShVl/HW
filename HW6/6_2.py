875caca78d89cc6b997b565019cef5166f2da835
Anna <k24i3@mail.ru>
2022-12-06 11:03:36 +0400

    6 HW

---
+++ HW6/6_2.py
@@ -0,0 +1,11 @@
+# Задайте список из n чисел последовательности (1+1/n)**n и выведите на экран их сумму.
+# n = int(input())
+# total = 0
+# for i in range(1,n+1):
+#     a = (1+1/i)**i
+#     print(a)
+#     total+=a
+# print(total)
+n = int(input('Введите число: '))
+print(list((1+1/i)**i for i in range(1,n+1)))
+print(sum(list((1+1/i)**i for i in range(1,n+1))))
---
+++ HW6/6_4.py
@@ -0,0 +1,22 @@
+# Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу
+# между максимальным и минимальным значением дробной части элементов.
+# s = [1.1, 1.2, 3.1, 5, 10.01]
+# s1 = []
+# import math
+# for i in range(len(s)):
+#     s1.append(s[i]-math.floor(s[i]))
+# s1_min = 1
+# for i in range(len(s1)):
+#     if s1[i]>0:
+#         if s1[i]<s1_min:
+#             s1_min=s1[i]
+# print(round((max(s1)-s1_min),10))
+import math
+def fract_part(num):
+    num=num-math.floor(num)
+    return num
+
+s = [1.1, 1.2, 3.1, 5, 10.01]
+s1 = list(map(fract_part, s))
+
+print(round(max(s1)-min(s1),10))
